bd19bd9ac3a33f2080188a6595d68ca55d77bcee
valentina.gorskova <valentina.gorskova@x5.ru>
2020-08-30 11:24:57 +0300

    add func scan

---
+++ src/ru/x5/Scan.java
@@ -0,0 +1,13 @@
+package ru.x5;
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        int pages = scanner.nextInt();
+
+        System.out.println(title);
+        System.out.println(pages + 1);
+    }
+}
